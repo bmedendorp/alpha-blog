37e27733092e64da8e8e00aa1a20fe9f496f1056
Brian Medendorp <brian@medendorponline.com>
2017-02-11 18:40:42 -0800

    Added total_steps_since_date method to SessionsController to return number of steps taken after a specified date

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,23 @@ class SessionsController < ApplicationController
     @todays_steps = @info['summary']['steps']
     @info = client.daily_activity_summary 'yesterday'
     @yesterdays_steps = @info['summary']['steps']
+    @total_steps = total_steps_since_date(client, '2017-02-09');
     @info = client.refresh_access_token(refresh_token)
   end
 
   def failure
   end
+
+  private
+  
+    # Return the total steps taken AFTER (since) the specified date
+    def total_steps_since_date(client, date)
+      date = Date.parse(date) + 1
+      activity_steps = client.activity_time_series resource: 'steps', start_date: date, end_date: 'today'
+      total_steps = 0
+      activity_steps['activities-steps'].each do |day|
+        total_steps += day['value'].to_i
+      end
+      return total_steps
+    end
 end
