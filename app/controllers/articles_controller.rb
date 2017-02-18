b78d68cadc698b1d4f8906d05c9e860e09b083dc
Brian Medendorp <brian@medendorponline.com>
2017-02-17 17:30:04 -0800

    Add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.find(6)
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def show
